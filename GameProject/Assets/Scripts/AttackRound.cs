e1a37003bf9575e6ac20887e7be4a4a0c9b53566
MaciejkuBiceps <mr.maciekson@gmail.com>
2015-12-01 20:05:19 +0100

    Do podstawówki

--- GameProject/Assets/Scripts/AttackRound.cs
+++ GameProject/Assets/Scripts/AttackRound.cs
@@ -9,29 +9,22 @@ public class AttackRound : MonoBehaviour {
 
     void Start () {
         listOfAttacks = new List<Attack>();
-        liczbaAtakow = 0;
     }
 	
 	public void addAttack(GameObject Attacker, GameObject Target)
     {
         listOfAttacks.Add(new Attack(Attacker, Target));
-        Debug.Log("Ilosc atakow w liscie: " + listOfAttacks.Count);
-        liczbaAtakow = listOfAttacks.Count;
     }
 
-    public void startAttack()
-    {
-        Debug.Log("Zaczynam Atak!" + listOfAttacks.Count);
-
+    public void startAttack() {
+        Debug.Log(listOfAttacks.Count);
         foreach (Attack attck in listOfAttacks)
         {
-            Debug.Log("bylem!");
             Statistics zycie = attck.Target.GetComponent<Statistics>();
             bool destroyBool = zycie.attack(attck.Attacker.GetComponent<Statistics>().atk);
 
             if (destroyBool) zycie.isAlive = false;       
         }
-
-        Debug.Log("Koniec Ataku!" + " pojemnsoc listy: " + listOfAttacks.Count);
+        listOfAttacks.Clear();
     }
 }
