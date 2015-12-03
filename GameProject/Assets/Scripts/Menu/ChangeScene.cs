c8410c251c1a426e8084db7e7b4efdd3d12b0645
MaciejkuBiceps <mr.maciekson@gmail.com>
2015-12-03 19:37:06 +0100

    Menu dodane

---
+++ GameProject/Assets/Scripts/Menu/ChangeScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    public void changeLevel()
+    {
+        Application.LoadLevel("Main");
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GameProject/Assets/Scripts/Menu/Exit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+
+    private bool close;
+	// Use this for initialization
+	void Start () {
+        close = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (close)
+            Application.Quit();
+
+    }
+
+    public void closeTheGameMethod()
+    {
+        close = true;
+    }
+}
